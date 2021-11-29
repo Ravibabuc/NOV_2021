c060d32aa7aaa7adc11c232255e78e783bd13c37
Ravibabuc <42927631+Ravibabuc@users.noreply.github.com>
2021-11-29 12:10:03 +0530

    Create Insert_missing_number_using_binary_search.cpp
    Sorted Insert Position
    Unsolved
    character backgroundcharacter
    Stuck somewhere?
    Ask for help from a TA & get it resolved
    Get help from TA
    
    Problem Description
    
    Given a sorted array A of size N and a target value B, return the index (0-based indexing) if the target is found.
    If not, return the index where it would be if it were inserted in order.
    
    NOTE: You may assume no duplicates in the array. Users are expected to solve this in O(log(N)) time.
    
    
    
    Problem Constraints
    
    1 <= N <= 106
    
    
    Input Format
    
    First argument is an integer array A of size N.
    Second argument is an integer B.
    
    
    Output Format
    
    Return an integer denoting the index of target value.
    
    
    Example Input
    
    Input 1:
    
    A = [1, 3, 5, 6]
     B = 5
    
    Input 2:
    
    A = [1]
     B = 1
    
    
    
    Example Output
    
    Output 1:
    
    2
    
    Output 2:
    
    0
    
    
    
    Example Explanation
    
    Explanation 1:
    
    The target value is present at index 2.
    
    Explanation 2:
    
    The target value is present at index 0.
    
    
    
    See Expected Output
    Enter your input as per the following guideline
    There are 2 lines in the input
    
    Line 1 ( Corresponds to arg 1 ) : An integer array. First number is the size S of the array. Then S numbers follow which indicate the elements in the array.
    
    Line 2 ( Corresponds to arg 2 ) : A single integer

---
+++ Insert_missing_number_using_binary_search.cpp
@@ -0,0 +1,79 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+int Search_number1(const vector<int> &A,int low, int high, int B)
+{
+    int mid ;
+  mid  =low + (high- low)/2;
+    if(low > high)
+        return mid;
+   
+    if(A[mid] == B)
+        return mid;
+    
+    if(B > A[mid]  )
+        return Search_number1 (A, mid+1, high, B);
+    else
+        return Search_number1 (A, low, mid-1, B);
+    
+    
+}
+
+int Search_number(const vector<int> &A,int low, int high, int B)
+{
+    if(low > high)
+        return -1;
+   int mid ;
+  mid  =low + (high- low)/2;
+    if(A[mid] == B)
+        return mid;
+    if( A[low] <= A[mid])
+    {
+        if(B >= A[low] && B <= A[mid] )
+            return Search_number(A, low, mid-1, B );
+        
+        return Search_number(A, mid +1, high, B );
+        
+    }
+     if(B >= A[mid]  && B <= A[high] )
+        return Search_number (A, mid+1, high, B);
+    else
+        return Search_number (A, low, mid-1, B);
+    
+    
+}
+
+
+int search(const vector<int> &A, int B) 
+{
+    int n = A.size();
+    int value =Search_number(A, 0, n, B);
+    return value;
+}
+
+
+int searchInsert(vector<int> &A, int B) {
+    int n = A.size();
+    int value =Search_number1(A, 0, n, B);
+    return value;
+}
+
+ 
+int main()
+{
+    
+    vector<int> A ={ 1, 3, 5, 6};
+    int B =7;
+    int C = searchInsert(A, B);
+    std::cout << C << std::endl;
+    return 0;
+}
